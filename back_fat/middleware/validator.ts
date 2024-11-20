4bcc099d0cca94baa539c63cd964f84765329bb9
LaurenceTest <reylaurencecapoy@gmail.com>
2024-11-20 15:18:35 +0800

    feat: Initial data validation

---
+++ back_fat/middleware/validator.ts
@@ -0,0 +1,162 @@
+import { validationResult, Result, Schema } from "npm:express-validator"
+import {Request, Response, NextFunction} from "npm:express"
+
+export const loginSchema:Schema = {
+    username:{
+        trim: true,
+        notEmpty: true,
+        escape: true,
+        isString: true,
+        isLength: { 
+            options: {
+                min : 3, max: 50,
+            },
+            errorMessage: "Username has invalid length"
+        },
+    },
+    email:{
+        trim: true,
+        notEmpty: true,
+        escape: true,
+        isEmail: true,
+        isLength: {options:{min:3}},
+    }
+}
+
+export const userCreateSchema:Schema = {
+    username:loginSchema.username,
+    email:loginSchema.email,
+    password:{
+        notEmpty: true,
+        escape: true,
+        isLength: {
+            options:{
+                min:8, max: 32
+            },
+            errorMessage: "Password minimum length is 8 and maximum of 50"
+        }            
+    },
+    birth_date:{
+        notEmpty: true,
+        isInt: true
+    },
+    gender:{
+        notEmpty: true,
+        matches: {
+            options: /^(male|female)$/i,
+            errorMessage: "Must be either 'Male' or 'Female'"
+        }
+    },
+    height:{
+        trim:true,
+        notEmpty: true,
+        escape:true,
+        isFloat: true
+    },
+    weight:{
+        trim:true,
+        notEmpty: true,
+        escape:true,
+        isFloat: true
+    },
+    main_goal:{
+        notEmpty: true,
+        matches: {
+            options: /^(LOSE WEIGHT|MAINTAIN WEIGHT|GAIN MUSCLE)$/i,
+            negated: true,
+            errorMessage: "Only valid values are: 'LOSE WEIGHT', 'MAINTAIN WEIGHT', 'GAIN MUSCLE'"
+        }
+    },
+    baseline_activity:{
+        notEmpty: true,
+        matches: {
+            options: /^(NOT ACTIVE|LIGHTLY ACTIVE|ACTIVE|VERY ACTIVE)$/i,
+            negated: true,
+            errorMessage: "Only valid values are: 'NOT ACTIVE', 'LIGHTLY ACTIVE', 'ACTIVE', 'VERY ACTIVE'"
+        }
+    },
+    weight_goal:{
+        trim: true,
+        notEmpty: true,
+        escape: true,
+        isFloat: true
+    }
+}
+
+export const workoutCreateSchema:Schema = {
+    name:{
+        notEmpty: true,
+        escape: true,
+        isString: true,
+        isLength: {
+            options:{
+                min: 3,
+                max: 50                
+            },
+            errorMessage: "Name minimum length is 3 and maximum of 50"
+        }
+    },
+    type:{
+        notEmpty: true,
+        escape: true,
+        matches:{
+            options: /^(CARDIOVASCULAR,STRENGTH TRAINING)$/i,
+            negated: true,
+            errorMessage: "Only valid values are: 'CARDIOVASCULAR', 'STRENGTH TRAINING'"
+        }
+    },
+    duration:{
+        trim: true,
+        notEmpty: true,
+        escape: true,
+        isTime: {
+            options:{
+                mode: 'withSeconds'
+            },
+            errorMessage: "Only allowed time format is HH:MM:SS"
+        }
+    },
+    weight:{
+        trim:true,
+        notEmpty: true,
+        escape:true,
+        isFloat: true
+    },
+    intensity:{
+        trim:true,
+        notEmpty: true,
+        escape:true,
+        isFloat: true
+    },
+}
+
+export const userUpdateSchema:Schema = {
+    username: {optional:true,...loginSchema.username},
+    email: {optional:true, ...loginSchema.email},
+    password: {optional:true, ...userCreateSchema.password},
+    birth_date: {optional:true,...userCreateSchema.birth_date},
+    height: {optional:true,...userCreateSchema.height},
+    gender: {optional:false,...userCreateSchema.gender}
+}
+
+export const goalUpdateSchema:Schema = {
+    main_goal: {optional:true,...userCreateSchema.main_goal},
+    baseline_activity: {optional:true,...userCreateSchema.baseline_activity},
+    weight_goal: {optional:true,...userCreateSchema.weight_goal}
+}
+
+export const workoutUpdateSchema:Schema = {
+    name: {optional:true,...workoutCreateSchema.name},
+    type: {optional:true,...workoutCreateSchema.type},
+    duration: {optional:true,...workoutCreateSchema.duration},
+    weight: {optional:true,... workoutCreateSchema.weight}
+}
+
+export const validateResult = (req:Request,res:Response,next:NextFunction)=>{
+    const result:Result = validationResult(req)
+    if(result.isEmpty())
+        next()
+    else
+        res.send({errors:result.array()})
+    
+}
--- back_fat/routes/user_routes.ts
+++ back_fat/routes/user_routes.ts
@@ -1,23 +1,26 @@
 // @ts-types="npm:@types/express"
 import {Router} from "npm:express";
+import {checkSchema} from "npm:express-validator"
 import { getUser, createUser, updateUser } from "../controllers/users_controller.ts";
 import { getPlans } from "../controllers/plans_controller.ts";
-import { getGoal } from "../controllers/goals_controller.ts";
+import { getGoal, updateGoal } from "../controllers/goals_controller.ts";
 import { createWorkout, getWorkouts, updateWorkout } from "../controllers/workouts_controller.ts";
+import { userCreateSchema, validateResult, workoutCreateSchema, userUpdateSchema,goalUpdateSchema, workoutUpdateSchema } from "../middleware/validator.ts";
 
 const router = Router()
 
 router.get("/user/:id/plan",getPlans)
 
 router.get("/user/:id/goal",getGoal)
+router.put("/user/update",checkSchema(goalUpdateSchema),validateResult,updateGoal)
 
 router.get("/user/:id/workout/:number", getWorkouts)
 router.get("/user/:id/workout/", getWorkouts)
-router.post("/user/:id/workout", createWorkout)
-router.put("/user/:id/workout", updateWorkout)
+router.post("/user/:id/workout", checkSchema(workoutCreateSchema),validateResult, createWorkout)
+router.put("/user/:id/workout", checkSchema(workoutUpdateSchema),validateResult, updateWorkout)
 
 router.get("/user/:id",getUser)
-router.put("/user", updateUser)
-router.post("/user", createUser)
+router.put("/user", checkSchema(userUpdateSchema),validateResult, updateUser)
+router.post("/user", checkSchema(userCreateSchema), validateResult, createUser)
 
 export default router
